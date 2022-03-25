b4b362484f20a71b662598ec732584219804bbcc
willzp <91528988+willzp@users.noreply.github.com>
2022-03-25 17:42:03 +0100

    Update CalculatorController.java

--- demo/CalculatorController.java
+++ demo/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Calculator  implements ErrorController {
+public class CalculatorController implements ErrorController {
 	
 	
 	plusCalculation plus = new plusCalculation();
